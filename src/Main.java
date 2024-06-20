eeada9bf08c4192ca2c8cc906496844b44333465
nihar3838 <141727428+nihar3838@users.noreply.github.com>
2024-06-20 16:07:48 +0530

    Snake and Ladder.java

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,51 @@
-public class Main {
-    public static int random(){
-        int rand = (int) ((Math.random() *6) +1);
-        return rand;
-    }
-    public static int random3(){
-        int rand = (int) ((Math.random() * 3) + 1);
-        return rand;
-    }
-    public static int snakeLadder(int position){
-        int rand = random();
-        int check = random3();
-        if(check == 2){
-            if(position + rand > 100){
-                return position;
-            }
-            position += rand;
+import java.lang.Math;
+public class Snake_ladder {
+    int WIN_CONDITION = 100;
 
-            snakeLadder(position);
-        } else if (check == 3) {
-            if(position - rand < 0){
-                position = 0;
-            }else{
-                position -= rand;
-            }
-        }else{;
-        }
-        return position;
+    public static int Diceroll() {
+        return (int) (Math.random() * 6) + 1;
+
     }
+
+    public static int Option() {
+         return (int) (Math.random() * 3) + 1;
+
+    }
+
     public static void main(String[] args) {
-        int WIN_POSITION = 100;
-        System.out.println("Welcome to Snake and Ladder game.");
-        int position1 = 0, diceRoll1 = 0, position2 = 0, diceRoll2 = 0;
-        while(position1 < WIN_POSITION && position2 < WIN_POSITION) {
-            position1 = snakeLadder(position1);
-            position2 = snakeLadder(position2);
-            diceRoll1++;
-            diceRoll2++;
-            System.out.println("Position of player 1 is " + position1 + " and the number of dice rolls are " + diceRoll1);
-            System.out.println("Position of player 2 is " + position2 + " and the number of dice rolls are " + diceRoll2);
-        }
-        if(position1 == WIN_POSITION){
-            System.out.println("Player 1 won the game.");
-        }else{
-            System.out.println("Player 2 won the game.");
-        }
+        int WIN_CONDITION = 100;
+        int COUNT =0;
+        int PLAYER_POSITION = 1;
+        int PREV_POS =1;
+        while (PLAYER_POSITION < WIN_CONDITION) {
+            int dice_roll = Diceroll();
+            int option = Option();
+            if(PLAYER_POSITION +dice_roll >WIN_CONDITION){
+                PLAYER_POSITION = PREV_POS;
+                COUNT++;
+            }
+            if (option == 1) {
+                PLAYER_POSITION += dice_roll;
+                COUNT++;
+            }
+            if (option == 2) {
+                PLAYER_POSITION -= dice_roll;
+                COUNT++;
+            }
+            if(PLAYER_POSITION <0){
+                PLAYER_POSITION =0;
+                COUNT++;
+            }
 
+            System.out.println("Dice roll is " + dice_roll);
+            System.out.println(option);
+            System.out.println("Player Position is " + PLAYER_POSITION);
+            if(PLAYER_POSITION ==WIN_CONDITION){
+                System.out.println("Player Win");
+                System.out.println("Dice Was Played " + COUNT + " Times");
+                break;
+            }
 
+        }
     }
 }
