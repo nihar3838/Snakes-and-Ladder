22f29454b704f896e79ce24ebed2ff49de73e95a
nihar3838 <niharkhot123@gmail.com>
2024-06-23 16:56:28 +0530

    Modifed Code to make win condition to 100

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,40 @@
 import java.lang.Math;
-public class SnakeLadder {
-    public static int Diceroll(){
-        int diceroll = (int)(Math.random()*6) +1;
+public class Main {
+    int win_position = 100;
+
+    public static int Diceroll() {
+        int diceroll = (int) (Math.random() * 6) + 1;
         return diceroll;
     }
+
+    public static int Option() {
+        int option = (int) (Math.random() * 3) + 1;
+        return option;
+    }
+
     public static void main(String[] args) {
-        int Win_condition =100;
-        int player_position =1;
-        int dice_role = Diceroll();
-        System.out.println(dice_role);
+        int Win_condition = 100;
+        int player_position = 1;
+        int prev_pos=1;
+        while (player_position < 100) {
+            int dice_roll = Diceroll();
+            int option = Option();
+            if(player_position+dice_roll >100){
+                player_position =prev_pos;
+            }
+            if (option == 1) {
+                player_position += dice_roll;
+            }
+            if (option == 2) {
+                player_position -= dice_roll;
+            }
+            if(player_position<0){
+                player_position =0;
+            }
+            if (option == 3) ;
+            System.out.println("Dice roll is " + dice_roll);
+            System.out.println(option);
+            System.out.println("Player Position is " + player_position);
+        }
     }
 }
--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -13,18 +13,28 @@ public class SnakeLadder {
     }
 
     public static void main(String[] args) {
-        int Win_condition =100;
-        int player_position =1;
-        int dice_roll = Diceroll();
-        int option = Option();
-        if(option ==1){
-            player_position +=dice_roll;
+        int Win_condition = 100;
+        int player_position = 1;
+        int prev_pos=1;
+        while (player_position < 100) {
+            int dice_roll = Diceroll();
+            int option = Option();
+            if(player_position+dice_roll >100){
+                player_position =prev_pos;
+            }
+            if (option == 1) {
+                player_position += dice_roll;
+            }
+            if (option == 2) {
+                player_position -= dice_roll;
+            }
+            if(player_position<0){
+                player_position =0;
+            }
+            if (option == 3) ;
+            System.out.println("Dice roll is " + dice_roll);
+            System.out.println(option);
+            System.out.println("Player Position is " + player_position);
         }
-        if(option ==2 ){
-            player_position -= dice_roll;
-        }
-        if(option == 3);
-        System.out.println("Dice roll is "+ dice_roll);
-        System.out.println("Player Position is "+player_position);
     }
 }
